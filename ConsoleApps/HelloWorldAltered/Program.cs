cce1f46e59ba082d1e0a37a7ee9a01198ba9526c
Larry Pallozzi <raijin22@outlook.com>
2022-07-21 08:50:56 -0400

    dotnet 6.0 test

---
+++ ConsoleApps/HelloWorldAltered/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldAltered
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("...some words go here I think...?");
+        }
+    }
+}
