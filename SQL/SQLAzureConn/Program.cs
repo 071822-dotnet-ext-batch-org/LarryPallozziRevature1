a23c2dacd8f94d20eda2f434847553a5a4287f7c
Larry <109549983+LarryPallozziRevature1@users.noreply.github.com>
2022-08-15 18:04:20 -0400

    sql

--- SQL/SQLAzureConn/Program.cs
+++ SQL/SQLAzureConn/Program.cs
@@ -7,7 +7,7 @@ namespace SQLAzureConn
     {
         static void Main(string[] args)
         {
-          SqlConnection conn = new SqlConnection("Azure Path");
+          SqlConnection conn = new SqlConnection("Server=tcp:larrypallozziazureserver.database.windows.net,1433;Initial Catalog=SQLDatabase1;Persist Security Info=False;User ID=LarryAzureLogin;Password=--------;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
           SqlCommand command = new SqlCommand("SELECT * FROM Customers", conn);
           conn.Open();
           SqlDataReader myReader = command.ExecuteReader();
