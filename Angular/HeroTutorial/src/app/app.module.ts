62ac2ea1d5d43f73a6b0149e292e745b43c81a77
Larry <109549983+LarryPallozziRevature1@users.noreply.github.com>
2022-09-08 20:56:09 -0400

    Hero tutorial

---
+++ Angular/HeroTutorial/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+  ],
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
