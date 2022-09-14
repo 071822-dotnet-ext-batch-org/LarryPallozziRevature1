0893883738215102c15156362d4248e170931b81
Larry <109549983+LarryPallozziRevature1@users.noreply.github.com>
2022-09-14 16:04:19 -0400

    files

---
+++ demos/DemoSPA/src/app/request-test/request-test.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RequestTestComponent } from './request-test.component';
+
+describe('RequestTestComponent', () => {
+  let component: RequestTestComponent;
+  let fixture: ComponentFixture<RequestTestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RequestTestComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RequestTestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
